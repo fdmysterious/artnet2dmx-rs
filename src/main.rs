8098d779cf9a22c388a8c2fa3ba6a2db79ca5f01
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2022-12-13 21:57:42 +0100

    Generate test array using from_fn magic

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::io::{self, Write};
 use std::time::Duration;
+use std::array;
 
 use serialport::{DataBits,StopBits};
 
 fn main() {
-    let port_name = "/dev/ttyUSB0";
-    let test_data = [255u8; 512];
+    let port_name           = "/dev/ttyUSB0";
+    let test_data: [u8;511] = array::from_fn(|i| (i%256) as u8);
+
 
     let builder = serialport::new(port_name, 250_000)
         .stop_bits(StopBits::One)
@@ -15,14 +17,13 @@ fn main() {
         panic!("Failed to open \"{}\". Error: {}", port_name, e);
     });
 
+    println!("Test data: {:?}", test_data);
+
     loop {
         port.set_break().unwrap();
         port.clear_break().unwrap();
         match port.write(&test_data) {
-            Ok(_) => {
-                println!("Ok");
-            }
-
+            Ok(_) => {}
             Err(ref e) if e.kind() == io::ErrorKind::TimedOut => {
                 println!("Timed out");
             }
