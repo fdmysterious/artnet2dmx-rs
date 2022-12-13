a2ac78da26560bcbe84e3af0d321903ca50a8c1a
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2022-12-13 21:51:50 +0100

    Send 511 bytes of 255 values to serial port. Got approx. between 350us and 500us using rt kernel

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,21 @@ use std::time::Duration;
 use serialport::{DataBits,StopBits};
 
 fn main() {
-    let test_str  = String::from("Hello world!\r\n");
     let port_name = "/dev/ttyUSB0";
+    let test_data = [255u8; 512];
 
-    let builder = serialport::new(port_name, 115_200)
+    let builder = serialport::new(port_name, 250_000)
         .stop_bits(StopBits::One)
         .data_bits(DataBits::Eight);
 
-    let mut port = builder.open_native().unwrap_or_else(|e| {
+    let mut port = builder.open().unwrap_or_else(|e| {
         panic!("Failed to open \"{}\". Error: {}", port_name, e);
     });
 
     loop {
-        match port.write(test_str.as_bytes()) {
+        port.set_break().unwrap();
+        port.clear_break().unwrap();
+        match port.write(&test_data) {
             Ok(_) => {
                 println!("Ok");
             }
@@ -27,7 +29,8 @@ fn main() {
 
             Err(e) => eprintln!("{:?}", e),
         }
-
-        std::thread::sleep(Duration::from_millis(1000));
     }
+
+    //port.set_break().unwrap();
+    //port.clear_break().unwrap();
 }
