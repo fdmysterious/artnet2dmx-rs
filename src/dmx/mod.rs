b6ce8d62b61eb054c7cf2caa5d7c90651ee9d8da
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2022-12-14 23:15:04 +0100

    WIP: Refactor into SerialDmxTransmitter structure

--- src/dmx/mod.rs
+++ src/dmx/mod.rs
@@ -1,24 +1,89 @@
 pub extern crate serialport as serial;
 
-use std::result::Result;
+use std::result::Result as StdResult;
+use std::io;
+
+// 8<----------------------------------
 
 #[derive(Debug)]
 pub enum DmxError {
-    IoError(serial::Error),
+    SerialError(serial::Error),
+    IoError(io::Error),
 }
 
-pub trait DmxTransmitter {
-    fn dmx_frame(&self, data: &[u8]) -> Result<(), DmxError>;
-    fn dmx_break(&self) -> Result<(), DmxError>;
+impl From<serial::Error> for DmxError {
+    fn from(err: serial::Error) -> Self {
+        Self::SerialError(err)
+    }
 }
 
-impl<T: serial::SerialPort> DmxTransmitter for T
-{
-    fn dmx_frame(&self, data: &[u8]) -> Result<(), DmxError> {
+impl From<io::Error> for DmxError {
+    fn from(err: io::Error) -> Self {
+        Self::IoError(err)
+    }
+}
+
+// 8<----------------------------------
+
+type Result<T> = StdResult<T, DmxError>;
+
+// 8<----------------------------------
+
+pub trait DmxTransmitter{
+    fn do_break(&self) -> Result<()>;
+    fn send_data(&mut self, buf: &[u8]) -> Result<()>;
+    fn flush(&mut self) -> Result<()>;
+
+    fn send_frame(&mut self, buf: &[u8]) -> Result<()> {
+        const start_code: [u8;1] = [0xFF];
+
+        self.do_break()?;
+        self.send_data(&start_code)?;
+        self.send_data(buf)?;
+        self.flush()?;
+
+        Ok(())
+    }
+}
+
+// 8<----------------------------------
+
+pub struct SerialDmxTransmitter<T: serial::SerialPort> {
+    port: T,
+}
+
+impl<T: serial::SerialPort> SerialDmxTransmitter<T> {
+    fn attach_to_port(port: T) -> Self {
+        Self {
+            port
+        }
+    }
+
+    fn do_break(&self) -> Result<()> {
+        self.port.set_break()?;
+        self.port.clear_break()?;
+        Ok(())
+    }
+    
+    fn send_data(&mut self, buf: &[u8]) -> Result<()> {
+        self.port.write(buf)?;
         Ok(())
     }
 
-    fn dmx_break(&self) -> Result<(), DmxError> {
+    fn flush(&mut self) -> Result<()> {
+        self.port.flush()?;
         Ok(())
     }
+}
+
+// 8<----------------------------------
+
+pub fn from_path(path: &str) -> Result<SerialDmxTransmitter<serial::TTYPort>> {
+    let builder = serial::new(path, 250_000)
+        .stop_bits(serial::StopBits::Two)
+        .data_bits(serial::DataBits::Eight);
+
+    Ok(SerialDmxTransmitter::attach_to_port(
+        builder.open_native()?
+    ))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,20 @@
 use std::time::Duration;
 use std::array;
 
-
 mod dmx;
-use crate::dmx::serial::{self, DataBits, StopBits};
-use crate::dmx::DmxTransmitter;
+use dmx::DmxTransmitter;
 
 fn main() {
     let port_name           = "/dev/ttyUSB0";
     let test_data: [u8;511] = array::from_fn(|i| (i%256) as u8);
 
-    let builder = serial::new(port_name, 250_000)
-        .stop_bits(StopBits::Two)
-        .data_bits(DataBits::Eight);
-
-    let mut port = builder.open_native().unwrap_or_else(|e| {
-        panic!("Failed to open \"{}\". Error: {}", port_name, e);
+    let test_port = dmx::from_path(port_name).unwrap_or_else(|e| {
+        panic!("Failed to open serial port: {:?}", e);
     });
 
-    println!("Test data: {:?}", test_data);
-
-    port.dmx_break().unwrap();
+    loop {
+        test_port.send_frame(&test_data).unwrap();
+    }
 
     //loop {
         //port.set_break().unwrap();
